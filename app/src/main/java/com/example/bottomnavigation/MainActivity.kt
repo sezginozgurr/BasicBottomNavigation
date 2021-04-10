51b03f93170184cfc6efdec5276dba403d34d8b8
sezginozgurr <sezginzgurr@gmail.com>
2021-04-11 01:29:30 +0300

    Finished Nav Controller/Bottom Navigation

--- app/src/main/java/com/example/bottomnavigation/MainActivity.kt
+++ app/src/main/java/com/example/bottomnavigation/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.bottomnavigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.bottomnavigation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navController = findNavController(R.id.containerFragment)
+
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 }
